4aea7268515f0bdfae6c8fbb4cd343554ee03903
Ali Nazari <ali.nazari@aut.ac.ir>
2020-07-14 16:53:30 +0430

    Updates

---
+++ precache-manifest.d065a8a2e0377377fb0defa76bedd4c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bb07d65982d40708a2165b7f44497c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6464f649e423c7a81a6e",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f07c3add1b4f537dd616",
+    "url": "./static/css/main.a6ed9de6.chunk.css"
+  },
+  {
+    "revision": "6464f649e423c7a81a6e",
+    "url": "./static/js/2.684a6f29.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.684a6f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07c3add1b4f537dd616",
+    "url": "./static/js/main.6d504f70.chunk.js"
+  },
+  {
+    "revision": "0f1fc8ec19a70eed78d9",
+    "url": "./static/js/runtime-main.2d6dd494.js"
+  }
+]);
